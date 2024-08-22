34758b9656295db51defd7cd6d07ff23b04a1127
Your Name <you@example.com>
2024-08-22 17:00:12 +0200

    try simple pio receiver

--- DEFCON32 Badge/micropython/ir_inifinite_reception.py
+++ DEFCON32 Badge/micropython/ir_inifinite_reception.py
@@ -1,9 +1,12 @@
 from rp2 import PIO, StateMachine, asm_pio
 from machine import Pin
 
-# Setup IR_SD pin
+# Setup IR_SD pin (for enabling/disabling the transceiver)
 ir_sd_pin = Pin(17, Pin.OUT)
 ir_sd_pin.value(0)  # Set to 0 to enable the transceiver
+
+# IR_RX is connected to GPIO 16
+ir_rx_pin = 16
 
 @asm_pio(set_init=PIO.IN_HIGH)
 def irda_rx():
@@ -15,8 +18,8 @@ def irda_rx():
     in_(pins, 1)    # Shift in the next data bit
     jmp(x_dec, "bitloop")  # Loop 8 times for 8 bits
 
-# Setup the state machine on GPIO pin connected to RXD
-sm = StateMachine(0, irda_rx, freq=9600 * 8, in_base=Pin(16))
+# Setup the state machine on GPIO pin connected to IR_RX
+sm = StateMachine(0, irda_rx, freq=9600 * 8, in_base=Pin(ir_rx_pin))
 
 sm.active(1)
 
@@ -25,6 +28,5 @@ while True:
         data = sm.get() & 0xff  # Get one byte of data
         print(chr(data))  # Print the received character
         
-    # Optionally, toggle the IR_SD pin based on some condition
-    # For example, disable the transceiver after a period of inactivity
+    # Optionally, you can disable the transceiver using IR_SD pin
     # ir_sd_pin.value(1)  # Set to 1 to disable the transceiver
